7cf5eeff1d6b4c6a81a52674cbbcac9b00f80f7e
TegarNH <tegarnaufalh@gmail.com>
2023-08-16 23:01:49 +0700

    ini pake cors

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	// "github.com/gin-contrib/cors"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
@@ -12,15 +12,8 @@ func main() {
 
 	router.Use(gin.Logger(), gin.Recovery())
 
-	// router.Use(cors.Default())
+	router.Use(cors.Default())
 
-	// config := cors.DefaultConfig()
-	// config.AllowWildcard = true
-	// config.AllowAllOrigins = true
-	// config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"}
-	// config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "Token"}
-
-	// router.Use(cors.New(config))
 	// router.Use(cors.New(config))
 
 	router.GET("/ping", func(c *gin.Context) {
