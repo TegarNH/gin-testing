f9b1174d76d3a368f78429f9d7d65a0b40178686
TegarNH <tegarnaufalh@gmail.com>
2023-08-16 22:22:09 +0700

    wdwdwd

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.New()
+
+	router.Use(gin.Logger(), gin.Recovery())
+
+	router.Use(cors.Default())
+
+	// config := cors.DefaultConfig()
+	// config.AllowWildcard = true
+	// config.AllowAllOrigins = true
+	// config.AllowMethods = []string{"GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"}
+	// config.AllowHeaders = []string{"Origin", "Content-Length", "Content-Type", "Token"}
+
+	// router.Use(cors.New(config))
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	router.Run("localhost:9000")
+}
